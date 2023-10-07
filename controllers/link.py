f6bf6ed35ff2264388b550dd6d979f293f2727ce
AFzOfficial <abol.mortal50@gmail.com>
2023-10-07 14:49:22 +0330

    add link controllers

---
+++ controllers/link.py
@@ -0,0 +1,53 @@
+from sqlalchemy.orm import Session
+
+from models.link import Link
+from schemas.link import LinkInput
+from utils.link import generate_random_string
+
+
+def get(db: Session, short: str) -> Link | None:
+    link = db.query(Link).filter(Link.short == short).first()
+    
+    if link:
+        return link
+    
+    return None
+
+
+def create(db: Session, request: LinkInput) -> Link:
+    while 1:
+        short = generate_random_string()
+
+        if get(db, short):
+            continue
+        else:
+            link = Link(address=request.address, short=short)
+            db.add(link)
+            db.commit()
+            db.refresh(link)
+            break
+
+    return link
+
+
+def update(db: Session, request: LinkInput, short: str) -> Link | None:
+    link = get(db, short)
+
+    if link:
+        link.address = request.address
+        db.commit()
+        db.refresh(link)
+        return link
+
+    return None
+
+
+def delete(db: Session, short: str) -> Link | None:
+    link = get(db, short)
+
+    if link:
+        db.delete(link)
+        db.commit()
+        return link
+    
+    return None
